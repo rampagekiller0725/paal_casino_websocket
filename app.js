48f39cef435336fb1969c03827e5d5135b0d9f92
rampagekiller0725 <113688176+rampagekiller0725@users.noreply.github.com>
2024-01-15 00:15:13 -0500

    io server

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
+    transports: ['polling', 'websocket', 'webtransport'],
     cors: {
-        origin: "https://paal-casino-websocket.vercel.app/",
+        origin: ["https://paal-casino-websocket.vercel.app/"],
     },
 });
 // io?.AsyncServer(async_mode='aiohttp', cors_allowed_origins='*')
